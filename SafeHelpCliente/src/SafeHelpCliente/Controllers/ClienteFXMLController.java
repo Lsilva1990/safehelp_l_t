3dc467c4ab619ab2641524804b066c59ee59e201
Lsilva1990 <lsilva.1990@alunos.utfpr.edu.br>
2021-08-10 23:34:46 -0300

    Criado SafeHelpCliente - Interface e controller

---
+++ SafeHelpCliente/src/SafeHelpCliente/Controllers/ClienteFXMLController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SafeHelpCliente.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author lsilva
+ */
+public class ClienteFXMLController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorpane;
+    @FXML
+    private Label lbTitulo;
+    @FXML
+    private TextField tfUser;
+    @FXML
+    private PasswordField pfPassword;
+    @FXML
+    private Button btnSair;
+    @FXML
+    private Button btnLogar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void sair(ActionEvent event) {
+    }
+
+    @FXML
+    private void Login(ActionEvent event) {
+    }
+    
+}
